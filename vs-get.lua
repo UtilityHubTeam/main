7118e57f86f1348ea64fd804dbb9d52fc030c10c
UtilityHub <106483689+UtilityHub@users.noreply.github.com>
2025-05-05 19:09:54 -0400

    Update and rename VS-GET.lua to vs-get.lua

---
+++ vs-get.lua
@@ -0,0 +1,12 @@
+--ＵｔｉｌｉｔｙＨｕｂ－ Ｎ －ＰＬＡＹ
+-- Credits: !Sunn
+-- Server Link: https://discord.com/invite/u3Az9gAruh
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()   
+
+local games = {
+    ["Ultimate Mining Tycoon"] = 18680867089,
+    ["Untitled Drill Game"] = 8770057349, -- coming soon.
+}
+
+loadstring(game:HttpGet("https://www.utilityhub.cloud/execution.lua"))();
